f1bbdde0db3c2ec8eb3ce1d9065b1a7dfbc47fb3
shailesz <sailesh.karki@sagarmatha.edu.np>
2022-03-29 13:32:07 +0545

    fix private key parse error

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const cors = require("cors");
 const multer = require("multer");
 const admin = require("firebase-admin");
 require("dotenv").config();
-console.log("app started");
+
 db.serialize(function () {
   db.run(
     "CREATE TABLE IF NOT EXISTS Users (UserId INTEGER PRIMARY KEY, Email TEXT, Password TEXT)"
@@ -22,7 +22,7 @@ admin.initializeApp({
   credential: admin.credential.cert({
     projectId: process.env.PROJECT_ID,
     clientEmail: process.env.CLIENT_EMAIL,
-    privateKey: process.env.PRIVATE_KEY,
+    privateKey: process.env.PRIVATE_KEY.replace(/\\n/g, "\n"),
   }),
   storageBucket: process.env.STORAGE_BUCKET,
 });
